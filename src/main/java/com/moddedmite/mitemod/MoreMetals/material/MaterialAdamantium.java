c7675a9ebaa0e93ba4100c105b879e763f1133c3
huanglanmu <huanglanmu147@gmail.com>
2026-03-22 11:44:20 +0800

    v1.1.0 add more metals

---
+++ src/main/java/com/moddedmite/mitemod/MoreMetals/material/MaterialAdamantium.java
@@ -0,0 +1,46 @@
+package com.moddedmite.mitemod.MoreMetals.material;
+
+import com.moddedmite.mitemod.MoreMetals.items.MMItems;
+import huix.glacier.api.extension.material.IArmorMaterial;
+import huix.glacier.api.extension.material.IArrowMaterial;
+import huix.glacier.api.extension.material.IRepairableMaterial;
+import huix.glacier.api.extension.material.IToolMaterial;
+import net.minecraft.EnumEquipmentMaterial;
+import net.minecraft.Item;
+import net.minecraft.Material;
+
+public class MaterialAdamantium extends Material implements IArmorMaterial, IArrowMaterial, IRepairableMaterial, IToolMaterial {
+    @Override
+    public float getChanceOfRecovery() {
+        return 0.7F;
+    }
+
+
+
+    public MaterialAdamantium(EnumEquipmentMaterial enum_crafting_material) {
+        super(enum_crafting_material);
+        this.setRequiresTool().setMetal(false).setMinHarvestLevel(5);
+    }
+
+    @Override
+    public float getDamageVsEntity() {
+        return 6.0F;
+    }
+
+    @Override
+    public int getProtection() {
+        return 10;
+    }
+
+    @Override
+    public Item getRepairItem() {
+        return MMItems.adamantium_nugget;
+    }
+
+    @Override
+    public float getHarvestEfficiency() {
+        return 5.0F;
+    }
+
+
+}
